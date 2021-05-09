8a6b30e5deb624c8a3ef31985bc9e69cee1cf26a
Adrien Navratil <adrien1975@live.fr>
2021-05-10 04:36:00 +0900

    Added Cargo template and isolate-sys crate generation

---
+++ isolate-sys/build.rs
@@ -0,0 +1,22 @@
+use std::path::PathBuf;
+
+fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+
+    let lib = pkg_config::Config::new().atleast_version("1.9.0").probe("isolate")
+        .expect("Unable to find isolate library using pkg-config");
+    let include_path = lib.include_paths.get(0)
+        .expect("Retrieved library has no include path");
+
+    let out_path = PathBuf::from(
+        std::env::var("OUT_DIR").expect("Cannot retrieve OUT_PATH environment variable")
+    );
+
+    bindgen::Builder::default()
+        .header(format!("{}", include_path.join("isolate.h").display()))
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Error while generating bindings")
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Cannot write bindings.rs file");
+}
